a02e0e27561d4ad96e467071a9b66c6a950abb65
jraspante <jraspante@MacBook-Pro-de-Jeremias.local>
2020-02-05 11:07:54 -0300

    Show message when user choose an invalid option

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,10 +3,9 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
+
         MenuApp menu = new MenuApp();
-
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book title in Bangalore!");
-
         menu.MenuOptions();
     }
 }
--- src/com/twu/biblioteca/MenuApp.java
+++ src/com/twu/biblioteca/MenuApp.java
@@ -7,11 +7,30 @@ public class MenuApp {
     public int MenuOptions()
     {
         Scanner scanner = new Scanner(System.in);
+        String [] options = new String[]{
+                "1 - List Books"
+        };
         int optionSelected = 0;
 
-        System.out.println("Please, type an option and then press enter");
-        System.out.println("1 - List Books");
-        optionSelected = Integer.parseInt(scanner.nextLine());
+        do {
+            System.out.println("Please, type an option number and then press enter");
+            for (int i = 0; i < options.length; i++){
+                System.out.println(options[i]);
+            }
+            try {
+                optionSelected = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) { }
+        }
+        while (!this.isValidOption(options, optionSelected));
         return optionSelected;
+
+    }
+
+    private boolean isValidOption(String [] menuOptions, int option){
+        for (int i = 0; i < menuOptions.length; i ++){
+            if(i == (option - 1)) return true;
+        }
+        System.out.println("Please select a valid option");
+        return false;
     }
 }
